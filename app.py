66eeb653e8dbdbedac4ac086902fe73d70df3e98
parasite-dad <hoifatck8@gmail.com>
2023-02-24 14:17:15 -0800

    add requirements.txt

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
-# from flask import Flask, flash, redirect, render_template, request, session, url_for
-# from flask_mysqldb import MySQL
-# import MySQLdb.cursors
-# app = Flask(__name__)
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'ck'
-# app.config['MYSQL_PASSWORD'] = 'ab1234'
-# app.config['MYSQL_DB'] = 'finance'
-# # app.config['MYSQL_PORT'] = 3306
-# mysql = MySQL(app)
-# # db = mysql.connection.cursor()
-# db = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
-
-# @app.route("/", methods=["GET", "POST"])
-# def index():
 
 import mysql.connector
 
